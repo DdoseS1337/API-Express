13f0055262093f0e23b16e807fdb5e5129072aef
DdoseS1337 <yaroslav.maksymchuk@gmail.com>
2022-10-19 23:39:59 +0300

    new middlewares

--- dist/main.js
+++ dist/main.js
@@ -11,11 +11,19 @@ const app_1 = require("./app");
 const exeption_filter_1 = require("./errors/exeption.filter");
 const users_controller_1 = require("./users/users.controller");
 require("reflect-metadata");
+const users_service_1 = require("./users/users.service");
+const config_service_1 = require("./config/config.service");
+const prisma_service_1 = require("./database/prisma.service");
+const users_repository_1 = require("./users/users.repository");
 exports.appBindings = new inversify_1.ContainerModule((bind) => {
-    bind(types_1.TYPES.ILogger).to(logger_service_1.LoggerSevice);
-    bind(types_1.TYPES.Application).to(app_1.App);
+    bind(types_1.TYPES.ILogger).to(logger_service_1.LoggerSevice).inSingletonScope();
     bind(types_1.TYPES.ExeptionFilter).to(exeption_filter_1.ExeptionFilter);
     bind(types_1.TYPES.UserController).to(users_controller_1.UserController);
+    bind(types_1.TYPES.UserService).to(users_service_1.UserService);
+    bind(types_1.TYPES.ConfigService).to(config_service_1.ConfigService).inSingletonScope();
+    bind(types_1.TYPES.PrismaService).to(prisma_service_1.PrismaService).inSingletonScope();
+    bind(types_1.TYPES.UsersRepository).to(users_repository_1.UsersRepository).inSingletonScope();
+    bind(types_1.TYPES.Application).to(app_1.App);
 });
 function bootstrap() {
     const appContainer = new inversify_1.Container();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,12 +10,18 @@ import { UserRegisterDto } from './dto/user-register.dto';
 import { UserService } from './users.service';
 import { HTTPError } from '../errors/http-error.class';
 import { ValidateMiddleware } from '../common/validate.middleware';
+import { sign } from 'jsonwebtoken';
+import { resolve } from 'path';
+import { IConfigService } from '../config/config.sevice.interface';
+import { IUserService } from './users.service.interface';
+import { AuthGuard } from '../common/auth.guard';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
 	constructor(
 		@inject(TYPES.ILogger) private loggerService: ILogger,
-		@inject(TYPES.UserService) private userService: UserService,
+		@inject(TYPES.UserService) private userService: IUserService,
+		@inject(TYPES.ConfigService) private configService: IConfigService,
 	) {
 		super(loggerService);
 		this.bindRoutes([
@@ -31,6 +37,12 @@ export class UserController extends BaseController implements IUserController {
 				func: this.login,
 				middlewares: [new ValidateMiddleware(UserLoginDto)],
 			},
+			{
+				path: '/info',
+				method: 'get',
+				func: this.info,
+				middlewares: [new AuthGuard()],
+			},
 		]);
 	}
 
@@ -43,7 +55,8 @@ export class UserController extends BaseController implements IUserController {
 		if (!result) {
 			return next(new HTTPError(401, 'Not correct data'));
 		}
-		this.ok(res, {});
+		const jwt = await this.signJWT(req.body.email, this.configService.get('SECRET'));
+		this.ok(res, { jwt });
 	}
 
 	async register(
@@ -56,5 +69,35 @@ export class UserController extends BaseController implements IUserController {
 			return next(new HTTPError(422, 'This user is already exist'));
 		}
 		this.ok(res, { email: result.email, id: result.id });
+	}
+
+	async info(
+		{ user }: Request<{}, {}, UserRegisterDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		const userInfo = await this.userService.getUserInfo(user);
+		this.ok(res, { email: userInfo?.email, id: userInfo?.id, name: userInfo?.name });
+	}
+
+	private signJWT(email: string, secret: string): Promise<string> {
+		return new Promise<string>((resolve, reject) => {
+			sign(
+				{
+					email,
+					iat: Math.floor(Date.now() / 1000),
+				},
+				secret,
+				{
+					algorithm: 'HS256',
+				},
+				(err, token) => {
+					if (err) {
+						reject(err);
+					}
+					resolve(token as string);
+				},
+			);
+		});
 	}
 }
